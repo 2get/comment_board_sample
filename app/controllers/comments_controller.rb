bb57e3d429fcbe2877b02619e50ff2411b9abbc1
Mayoto Yoshida <2getjp@gmail.com>
2012-09-03 11:33:25 +0900

    Improve change post form location

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   # GET /comments.json
   def index
     @comments = Comment.all
+    @comment = Comment.new
 
     respond_to do |format|
       format.html # index.html.erb
@@ -44,10 +45,10 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to comments_path, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
-        format.html { render action: "new" }
+        format.html { render action: "index" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
