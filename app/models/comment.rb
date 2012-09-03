3c6775be22ed1a7d36ccb6b5a1aaaf725f1c1608
Mayoto Yoshida <2getjp@gmail.com>
2012-09-03 10:55:29 +0900

    Add validate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :body, :name, :title
+  attr_accessible :name, :title, :body
+
+  validates :name,  presence: true
+  validates :title, presence: true
+  validates :body,  presence: true
+
+  default_scope order: 'comments.created_at DESC'
 end
