4bc37cea2c47bf2c647536a7d1819d815623560a
Mayoto Yoshida <2getjp@gmail.com>
2012-09-03 10:52:44 +0900

    Fix root path

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ CommentBoard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'comments#index'
 
   # See how all your routes lay out with "rake routes"
 
