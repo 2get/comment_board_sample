cadb0dc94a4f0bfbc94526070ccce11d1b937e19
Mayoto Yoshida <2getjp@gmail.com>
2012-09-03 10:44:11 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommentBoard::Application.config.secret_token = 'aabdaccc1908cc27fff5af761364116caa37397404b68ccf0ec5ea4848d48ccbf99e9e6f773d666c4441a30aff11d07398b87809fef90829d3548f545c4603b4'
